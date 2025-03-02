c7d13568d3e0a3a08c6c4b79dca59d1aece212bd
someone <ootinnyoo@outlook.com>
2025-03-01 20:01:22 -0500

    use single client instead of creating reqwest client for all requests

--- src/audio.rs
+++ src/audio.rs
@@ -1,28 +1,30 @@
 use std::io::Cursor;
 use tokio::sync::mpsc::Receiver;
-use reqwest::get;
+use reqwest::Client;
 use rodio::{Decoder, Sink};
 use anyhow::{Context, Error};
 
 pub async fn audio_loop(mut rx: Receiver<String>, sink: Sink) -> Result<(), Error> {
+    let client = Client::new();
     loop {
         let input = rx.recv().await
             .context("Audio channel closed")?;
         match input.as_str() {
             "died" => sink.set_volume(0.5),
             "left" => sink.stop(),
-            _ => play_audio(input, &sink).await?,
+            _ => play_audio(input, &client, &sink).await?,
         }
     }
 }
 
-pub async fn play_audio(url: String, sink: &Sink) -> Result<(), Error> {
+pub async fn play_audio(url: String, client: &Client, sink: &Sink) -> Result<(), Error> {
     println!("\nGot request to play audio {}", url);
     // Stop the current playback, if any
     sink.stop();
 
     // Get response from URL
-    let response = get(url)
+    let response = client.get(url)
+        .send()
         .await?
         .bytes()
         .await?;
