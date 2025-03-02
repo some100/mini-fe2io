06885862e6915eaaf3029e2d11ed7f38e6e46427
someone <ootinnyoo@outlook.com>
2025-03-01 19:22:49 -0500

    significantly refactor program

--- src/main.rs
+++ src/main.rs
@@ -2,67 +2,51 @@ mod audio;
 pub mod json_processor;
 
 use std::io::Write;
-use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::task;
+use tokio::sync::mpsc::channel;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 use futures_util::{StreamExt, SinkExt};
-use cpal::traits::{DeviceTrait, HostTrait};
+use rodio::{OutputStream, Sink};
+use anyhow::{Context, Error};
 
-// Initialize Audio
-
-
-//use url::Url;
 #[tokio::main]
-async fn main() {
-     // Get the default audio host
-     let host = cpal::default_host();
-
-     // Get the default input and output devices
-     let output_device = host.default_output_device().expect("No output device available");
-     println!("Output Device: {:?}", output_device.name().unwrap());
-
+async fn main() -> Result<(), Error> {
     let mut input = String::new();
-    print!("Please enter your Roblox username!: ");
-    std::io::stdout().flush().unwrap();
-    std::io::stdin().read_line(&mut input).expect("Unhandled Exception!");
+    print!("Please enter your Roblox username: ");
+    std::io::stdout().flush()?;
+    std::io::stdin().read_line(&mut input)?;
     let username = input.trim().to_string();
-    println!("Your Roblox username is {}", username);
-    let _ = task::spawn(websocket_connect(username)).await;
+    websocket_connect(username).await?;
+    Ok(())
 }
 
-async fn websocket_connect(username: String) {
-    let url = url::Url::parse("ws://client.fe2.io:8081").unwrap();
+async fn websocket_connect(username: String) -> Result<(), Error> { // this does way more than websocket connect lol
+    let (ws_stream, _response) = connect_async("ws://client.fe2.io:8081").await?;
+    println!("Connection established");
 
-    let (ws_stream, _response) = connect_async(url).await.expect("Failed to connect");
-    println!("Connection Established");
+    let (mut write, mut read) = ws_stream.split();
+    write.send(Message::Text(username)).await?;
 
-    let (mut write, read) = ws_stream.split();
+    println!("Sent username to server");
 
-    println!("sending");
+    let (tx, rx) = channel(2);
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+    task::spawn(audio::audio_loop(rx, sink));
 
-    write.send(Message::Text(username)).await.unwrap();
-
-    println!("sent");
-
-    let player = Arc::new(audio::AudioPlayer::new());
-
-    let read_future = read.for_each(|message| {
-        let player = Arc::clone(&player);
-        async move {
-        
+    loop {
+        let message = read.next().await
+            .context("Couldn't receive messages from server")?;
         println!("receiving...");
-        let data = message.unwrap().into_data();
-        tokio::io::stdout().write(&data).await.unwrap();
+        let data = message?.into_data();
+        tokio::io::stdout().write(&data).await?;
         if let Ok(string_data) = String::from_utf8(data.clone()) {
             println!("received: {}", string_data);
             // Process the data
-            json_processor::process_data(&string_data, &player).await;
+            json_processor::process_data(&string_data, tx.clone()).await?;
         } else {
             println!("received: Invalid UTF-8 data");
         }
-}});
-    
-
-    read_future.await;
+    }
 }
